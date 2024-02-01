3b53a4254def3e2110685fe543034be14c9e5df7
André Soares da Silva <146672413+AndreSoaresBR@users.noreply.github.com>
2024-02-01 18:29:22 -0300

    10_SSMS_Variaveis.sql

--- 10_SSMS_Variaveis.sql
+++ 10_SSMS_Variaveis.sql
@@ -34,29 +34,45 @@ select 431.0/23 as 'Divisão' -- resultado com numero decimal
 select 'André ' + 'Soares' as 'Nome'
 -----------------------------------------------
 Visualizando o tipo
-select sql_variant_property(10, 'BaseType')
-select sql_variant_property(49.50, 'BaseType')
-select sql_variant_property('Marcus', 'BaseType')
-select sql_variant_property('20/06/2021', 'BaseType')
+select sql_variant_property (10, 'basetype')
+select sql_variant_property (49.50, 'basetype')
+select sql_variant_property ('Marcus', 'basetype')
+select sql_variant_property ('20/06/2021', 'basetype')
 -----------------------------------------------
-Convertendo o tipo
-select cast (21.5 as int)
-select cast (21.45 as float)
+Convertendo e visualizando o tipo
+select cast (21.45 as int)
+select sql_variant_property (cast (21.45 as int), 'basetype')
+select cast (21 as float)
+select sql_variant_property (cast (21 as float), 'basetype')
 select cast (18.7 as varchar)
+select sql_variant_property (cast (18.7 as varchar), 'basetype')
 select cast ('15.6' as float)
+select sql_variant_property (cast ('15.6' as float), 'basetype')
+select cast ('20/06/2021' as date)
+select sql_variant_property (cast ('20/06/2021' as date), 'basetype')
 select cast ('20/06/2021' as datetime)
 select sql_variant_property (cast ('20/06/2021' as datetime), 'basetype')
 select 'O preço do pastel é R$ ' + cast (30.99 as varchar(50))
 select cast ('20/06/2021' as datetime) + 1
 -----------------------------------------------
 Formatação
-select format(1000, 'N')
-select format(1000, 'G')
-select format(123456789, '###-##-###')
-select format(cast('21/03/2021' as datetime), 'dd/MM/yyy')
-select format(cast('21/03/2021' as datetime), 'dd/MMM/yyy')
-select format(cast('21/03/2021' as datetime), 'dd/MMMMM/yyy')
-select format(cast('21/03/2021' as datetime), 'dd')
-select format(cast('21/03/2021' as datetime), 'ddd')
-select format(cast('21/03/2021' as datetime), 'dddd')
+select format (1000, 'N')
+select format (1000, 'G')
+select format (123456789, '###-##-####')
+select format (cast ('21/03/2021' as datetime), 'dd/MM/yyy')
+select format (cast ('21/03/2021' as datetime), 'dd/MMM/yyy')
+select format (cast ('21/03/2021' as datetime), 'dd/MMMMM/yyy')
+select format (cast ('21/03/2021' as datetime), 'dd')
+select format (cast ('21/03/2021' as datetime), 'ddd')
+select format (cast ('21/03/2021' as datetime), 'dddd')
+select format (cast ('21/03/2021' as datetime), 'MM')
+select format (cast ('21/03/2021' as datetime), 'MMM')
+select format (cast ('21/03/2021' as datetime), 'MMMM')
+select 'A data de validede do produto é: ' + format ( cast ('17/04/2022' as datetime), 'dd/MMM/yyy')
+-----------------------------------------------
+Funções de Arredondamento
+SELECT ROUND (18.739130, 2)    -- seleciona quantas casas decimais e arredonda de acordo com as regras da matematica
+SELECT ROUND (18.739130, 2, 1) -- seleciona quantas casas decimais e ignora as regras da matematica
+SELECT FLOOR (18.739130)       -- arredonda para baixo sem casas decimais
+SELECT CEILING (18.739130)     -- arredonda para cima sem casas decimais
 -----------------------------------------------
