3c0f7a20a462acc81decb5eb73275c6b9e1ce4cd
André Soares da Silva <146672413+AndreSoaresBR@users.noreply.github.com>
2024-02-06 22:08:14 -0300

    10_SSMS_Variaveis.sql

--- 10_SSMS_Variaveis.sql
+++ 10_SSMS_Variaveis.sql
@@ -23,6 +23,7 @@ Exemplos:
 '01/01/2021', '23/03/2021'
 -----------------------------------------------
 Visualizando operações
+
 select 10 as 'Numero'
 select 'André' as 'Nome'
 select '21/06/2011' as 'Data'
@@ -34,12 +35,14 @@ select 431.0/23 as 'Divisão' -- resultado com numero decimal
 select 'André ' + 'Soares' as 'Nome'
 -----------------------------------------------
 Visualizando o tipo
+
 select sql_variant_property (10, 'basetype')
 select sql_variant_property (49.50, 'basetype')
 select sql_variant_property ('André', 'basetype')
 select sql_variant_property ('20/06/2021', 'basetype')
 -----------------------------------------------
 Convertendo e visualizando o tipo
+
 select cast (21.45 as int)
 select sql_variant_property (cast (21.45 as int), 'basetype')
 select cast (21 as float)
@@ -57,6 +60,7 @@ select 'O preço do pastel é R$ ' + cast (30.99 as varchar(50))
 select cast ('20/06/2021' as datetime) + 1
 -----------------------------------------------
 Formatação
+
 select format (1000, 'N')
 select format (1000, 'G')
 select format (123456789, '###-##-####')
@@ -73,12 +77,14 @@ select format (cast ('21/03/2021' as datetime), 'MMMM')
 select 'A data de validede do produto é: ' + format ( cast ('17/04/2022' as datetime), 'dd/MMM/yyy')
 -----------------------------------------------
 Funções de Arredondamento
+
 select round (18.739130, 2)    -- seleciona quantas casas decimais e arredonda de acordo com as regras da matematica
 select round (18.739130, 2, 1) -- seleciona quantas casas decimais e ignora as regras da matematica
 select floor (18.739130)       -- arredonda para baixo sem casas decimais
 select ceiling (18.739130)     -- arredonda para cima sem casas decimais
 -----------------------------------------------
 Declarando uma variavel
+
 declare @variavel tipo
 set @variavel = valor
 
@@ -98,7 +104,8 @@ declare @date date
 set @date = '01/02/2024'
 select @date as Data
 
-Outra opção
+Outra opção simplificada
+
 declare
 	@idade int = 10,
 	@preco float = 10.89,
@@ -115,8 +122,9 @@ declare
 	@preco float = 89.99
 
 select @quantidade * @preco as Faturamento
------------------------------------------------
+
 Exemplos
+
 /*
 Aplique um desconto de 10% em todos os preços dos produtos.
 Sua consulta final deve conter as colunas ProductKey, ProductName, UnitPrice 
@@ -125,17 +133,19 @@ e Preço com Desconto.
 
 declare @desconto float = 0.10
 
-select
+ select
 	ProductKey as 'ID',
-	ProductName as 'Nome do produto',
-	UnitPrice as 'Preço unitario',
-	UnitPrice * (1 - @desconto) as 'Preço com desconto'
-from
+	ProductName as 'Produto',
+	UnitPrice as 'Preço',
+	round (UnitPrice * (1 - @desconto), 2) as 'Preço com Desconto',
+	round (UnitPrice * @desconto, 2) as 'Valor do Desconto'
+from	
 	DimProduct
 
 /*
 Crie uma variavel de data para melhorar a consulta abaixo.
 */
+
 declare @varData datetime = '01/01/1980'
 
 select 
@@ -161,4 +171,80 @@ where
 	BirthDate >= @varData
 order by 
 	Nascimento
+-----------------------------------------------
+Armazenando o resultado de um select em uma variavel
+
+/*
+Crie uma variavel para armazenar a quantidade total de
+funcionarios da tabela DimEmployee.
+*/
+
+declare @qtdfuncionarios int = (select count (*) from DimEmployee)
+
+select @qtdfuncionarios as 'Qtd funcionarios'
+
+/*
+Crie uma variavel para armazenar a quantidade total de lojas com o status Off.
+*/
+
+declare @lojasoff int = (select count (*) from DimStore where status = 'Off')
+
+select @lojasoff as 'Lojas Off'
+-----------------------------------------------
+Printando uma mensagem na tela
+
+/*
+Printe na tela a quantidade de lojas On e a quantidade  de lojas Off da tabela DimStore.
+Utilize variaveis para isso.
+*/
+
+set nocount on
+
+declare
+	@lojason int = (select count (*) from DimStore where status = 'on'),
+	@lojasoff int = (select count (*) from DimStore where status = 'off')
+
+select
+	@lojason as 'Lojas On',
+	@lojasoff as 'Lojas Off'
+
+print 'O total de lojas abertas é de ' + cast (@lojason as varchar (30))
+print 'O total de lojas fechadas é de ' + cast (@lojasoff as varchar (30))
+-----------------------------------------------
+Armazenando em uma variavel um registro de uma consulta
+
+/*
+Qual é o nome do produto que teve a maior quantidade vendida em uma unica venda da tabela FactSales?
+*/
+
+declare
+	@idprodutomaisvendido int,
+	@nomeprodutomaisvendido varchar (100),
+	@totalmaisvendido int
+
+select top (1)
+	@idprodutomaisvendido = DimProduct.ProductKey,
+	@nomeprodutomaisvendido = dimproduct.ProductName,
+	@totalmaisvendido = SalesQuantity
+from
+	FactSales
+inner join
+	DimProduct on
+	DimProduct.ProductKey = FactSales.ProductKey
+order by
+	SalesQuantity desc
+
+print 'ID do produto: ' + cast (@idprodutomaisvendido as varchar (30))
+print 'Nome do Produto: ' + cast (@nomeprodutomaisvendido as varchar (100))
+print 'Quantidade de venda: ' + cast (@totalmaisvendido as varchar (30))
+
+select
+	ProductKey as 'ID',
+	@totalmaisvendido as 'Total vendido',
+	ProductName as 'Nome'
+from
+	DimProduct
+where
+	ProductKey = @idprodutomaisvendido
+-----------------------------------------------
 
